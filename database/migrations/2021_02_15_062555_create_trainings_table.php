0d575146993c5a2936e5ab716ab280d5e4142941
Tarmizi Sanusi <realtarmizisanusi@gmail.com>
2021-02-15 15:44:52 +0800

    Day 1: trainings migration

---
+++ database/migrations/2021_02_15_062555_create_trainings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trainings', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+
+            //define FK - foreign key
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trainings');
+    }
+}
