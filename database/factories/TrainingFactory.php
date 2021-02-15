32d5904312d3e2007a5d4248aa1d11f8e4eb9ee5
Tarmizi Sanusi <realtarmizisanusi@gmail.com>
2021-02-15 15:45:09 +0800

    Day 1: training factory

---
+++ database/factories/TrainingFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Training;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TrainingFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Training::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->name,
+            'description' => $this->faker->text,
+            'user_id' => \App\Models\User::factory()->create()->id
+        ];
+    }
+}
